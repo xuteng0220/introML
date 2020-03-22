793ac787380ab22c36d0774287e34e218915bac9
xuteng <xuteng0220@gmail.com>
2020-03-22 17:14:38 +0800

    mac20200322

--- ch2_supervised_learning.py
+++ ch2_supervised_learning.py
@@ -562,16 +562,16 @@ gbrt.score(X_train, y_train)
 gbrt.score(X_test, y_test)
 
 # lower learning_rate can reduce overfitting
-grbt = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
-grbt.fit(X_train, y_train)
-grbt.score(X_train, y_train)
-grbt.score(X_test, y_test)
+gbrt = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gbrt.fit(X_train, y_train)
+gbrt.score(X_train, y_train)
+gbrt.score(X_test, y_test)
 
 # fewer tree depth can reduce overfitting
-grbt = GradientBoostingClassifier(random_state=0, max_depth=1)
-grbt.fit(X_train, y_train)
-grbt.score(X_train, y_train)
-grbt.score(X_test, y_test)
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+gbrt.score(X_train, y_train)
+gbrt.score(X_test, y_test)
 
 
 plot_feature_importances_cancer(gbrt)
@@ -674,6 +674,7 @@ from sklearn.svm import SVC
 
 X, y = mglearn.tools.make_handcrafted_dataset()
 # train a kernel SVM. parameter gamma controls the width of the Gaussian kernel. it determines the scale for points how close together. parameter C is a regularization parameter. it limits the importance of each support vector(value of support vector in dual_coef_)
+SVC?
 svm = SVC(kernel='rbf', C=10, gamma=0.1).fit(X, y)
 # plot the decision boundry
 mglearn.plots.plot_2d_separator(svm, X, eps=.5)
@@ -728,6 +729,244 @@ plt.yscale('log')
 
 
 #### preprocessing data for SVMs
-
+# minimum value per feature
 min_on_training = X_train.min(axis=0)
+# range of each feature(max - min)
+range_on_training = (X_train - min_on_training).max(axis=0)
+# scale the training data to 0-1
+X_train_scaled = (X_train - min_on_training) / range_on_training
+X_train_scaled.max(axis=0)
+X_train_scaled.min(axis=0)
+
+# use the same transformation on test set
+X_test_scaled = (X_test - min_on_training) / range_on_training
+
+svc = SVC()
+svc.fit(X_train_scaled, y_train)
+svc.score(X_train_scaled, y_train)
+svc.score(X_test_scaled, y_test)
+
+
+svc1000 = SVC(C=1000)
+svc1000.fit(X_train_scaled, y_train)
+svc1000.score(X_train_scaled, y_train)
+svc1000.score(X_test_scaled, y_test)
+
+
+
+### Neural Networks(Deep Learning)
+#### the neural network model
+# multilayer perceptrons(MLPs) are known as (vanilla) feed-forward neural networks
+# MLPs can be viewed as generalizations of linear models that perform multiple stages of preprocessing to come to a decision
+
+# a graph illustration of linear model
+display(mglearn.plots.plot_logistic_regression_graph())
+
+# graph illustrations of MLPs
+display(mglearn.plots.plot_single_hidden_layer_graph())
+mglearn.plots.plot_two_hidden_layer_graph()
+
+line = np.linspace(-3, 3, 100)
+plt.plot(line, np.tanh(line), label='tanh')
+plt.plot(line, np.maximum(line, 0), label='relu')
+plt.legend(loc='best')
+plt.xlabel('x')
+plt.ylabel('relu(x), tanh(x)')
+
+
+#### Tuning nerural networks
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+
+
+X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+mlp = MLPClassifier(algorithm='l-bfgs', random_state=0).fit(X_train, y_train)
+# decision boundry
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3)
+# scatter plot
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel('feature 0')
+plt.ylabel('feature 1')
+
+
+# mlp model with 10(100, by default)hidden nodes and 1 layer
+mlp = MLPClassifier(algorithm='l-bfgs', random_state=0, hidden_layer_sizes=[10])
+mlp.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel('feature 0')
+plt.ylabel('feature 1')
+
+# using two hidden layers, with 10 nodes each
+mlp = MLPClassifier(algorithm='l-bfgs', random_state=0, hidden_layer_sizes=[10, 10])
+mlp.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel('featrue 0')
+plt.ylabel('featrue 1')
+
+# two hidden layers, 10 nodes each, with tanh nonlinearity(default, relu-rectifying nonlinearity, or rectified linear unit)
+mlp = MLPClassifier(algorithm='l-bfgs', activation='tanh', random_state=0, hidden_layer_sizes=[10, 10])
+mpl.fit(X_train, y_train)
+mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3)
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.xlabel('featrue 0')
+plt.ylabel('featrue 1')
+
+# mlp uses an l2 penalty to shrink the weights toward zero
+# parameter alpha in mlp controls the model complexity, large alpha makes it more general
+fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+for axx, n_hidden_nodes in zip(axes, [10, 100]):
+	for ax, alpha in zip(axx, [0.0001, 0.01, 0.1, 1]):
+		mlp = MLPClassifier(algorithm='l-bfgs', random_state=0, hidden_layer_sizes=[n_hidden_nodes, n_hidden_nodes], alpha=alpha)
+		mlp.fit(X_train, y_train)
+		mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3, ax=ax)
+		mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train, ax=ax)
+		ax.set_title('n_hidden_nodes=[{}, {}]\nalpha={:.4f}'.format(n_hidden_nodes, n_hidden_nodes, alpha))
+
+# parameter random_state in mlp, set initial weights randomly, which will affect the model
+fig, axes = plt.subplots(2, 4, figsize=(20, 8))
+for i, ax in enumerate(axes.ravel()):
+	mlp = MLPClassifier(algorithm='l-bfgs', random_state=i, hidden_layer_sizes=[100, 100])
+	mlp.fit(X_train, y_train)
+	mglearn.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=0.3, ax=ax)
+	mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train, ax=ax)
+	ax.set_title('n_hidden_nodes=[100, 100]\n random_state={}'.format(i))
+
+
+# apply MLPClassifier to the breast cancer datsets
+from sklearn.datsets import load_breast_cancer
+cancer = load_breast_cancer()
+
+print('cancer data per-feature maxima:\n{}'.format(cancer.data.max(axis=0)))
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, random_state=0)
+
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+
+mlp.score(X_train, y_train)
+mlp.score(X_test, y_test)
+
+
+# it's better to scale the original data before applying mlp
+# mean value of per feature on the training data
+mean_on_train = X_train.mean(axis=0)
+# standard deviation of per feature
+std_on_train = X_train.std(axis=0)
+# scale the training data to be 0 mean and 1 std
+X_train_scaled = (X_train - mean_on_train) / std_on_train
+# scale the test data the same way using train mean and std
+X_test_scaled = (X_test - mean_on_train) / std_on_train
+
+mlp = MLPClassifier(random_state=0)
+mlp.fit(X_train_scaled, y_train)
+
+mlp.score(X_train_scaled, y_train)
+mlp.score(X_test_scaled, y_test)
+
+
+# compare with SVM classifier
+from sklearn.svm import SVC
+svc = SVC().fit(X_train_scaled, y_train)
+svc.score(X_train_scaled, y_train)
+svc.score(X_test_scaled, y_test)
+
+# increase the number of iterations
+# about algorithm adam?
+mlp = MLPClassifier(max_iter=1000, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+mlp.score(X_train_scaled, y_train)
+mlp.score(X_test_scaled, y_test)
+
+# increase alpha makes the model more general, alpha=0.0001 by default
+mlp = MLPClassifier(max_iter=1000, alpha=1, random_state=0)
+mlp.fit(X_train_scaled, y_train)
+mlp.score(X_train_scaled, y_train)
+mlp.score(X_test_scaled, y_test)
+
+plt.figure(figsize=(20, 5))
+plt.imshow(mlp.coefs_[0], interpolation='none', cmap='viridis')
+plt.yticks(range(30), cancer.feature_names)
+plt.xlabel('columns in weight matrix')
+plt.ylabel('input feature')
+plt.colorbar()
+
+### uncertainty estimates from classifiers
+
+# you are not only interested in which class a classifier predicts for a certain test point, but also how certain it is that this is the right class
+
+# scikit-learn obtains uncertainty estimates from classifiers using decision_function and predict_proba
+
+from sklearn.ensemble import  GradientBoostingClassifier
+from sklearn.ensemble make_blobs, make_circles
+X, y = make_circles(noise=0.25, factor=o.5, random_state=1)
+X
+y
+
+# rename the y label using blue and red
+y_named = np.array(['blue', 'red'])[y]
+
+X_train, X_test, y_train_named, y_test_named, y_train, y_test = train_test_split(X, y_named, y, random_state=0)
+
+
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train_named)
+
+# model method decision_function has shape (n_samples,)
+gbrt.decision_function(X_test)
+X_test.shape
+gbrt.decision_function(X_test).shape
+
+# for binary classification, the negative class is the first entry of the classes_ attribute
+gbrt.decision_function(X_test) > 0
+gbrt.predict(X_test)
+
+
+# make true/false into 0 and 1
+greater_zero = (gbrt.decision_function(X_test) > 0).astype(int)
+pred = gbrt.classes_[greater_zero]
+
+pred
+gbrt.predict(X_test)
+# these two are the same
+np.all(pred == gbrt.predict(X_test))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
